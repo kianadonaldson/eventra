833c73b37d1555b47b63f45808e08cde12eca343
Kiana Donaldson <kianakellidonaldson@gmail.com>
2026-06-23 15:47:14 -0700

    Implement infinite scroll for event listings

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import CategorySelect from './components/CategorySelect';
 import EventCard from './components/EventCard';
 import styled from 'styled-components';
 
 const Title = styled.h1`
+    font-size: 60px;
     font-family: system-ui; sans-serif;
+    font-style: italic;
+    margin-top: 10px;
+    margin-bottom: 0px;
 `;
 
 const Cards = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
-    grid-auto-rows: 300px;
+    grid-auto-rows: 270px;
     gap: 15px;
     font-family: system-ui; sans-serif;
 `;
@@ -18,6 +22,8 @@ const Cards = styled.div`
 export default function App() {
     const [events, setEvents] = useState([]);
     const [category, setCategory] = useState('All');
+    const [visibleCount, setVisibleCount] = useState(6);
+    const loadMoreRef = useRef(null);
 
     const fetchEvents = () => {
         fetch('/api/events')
@@ -34,22 +40,56 @@ export default function App() {
             ? events
             : events.filter(event => event.category === category);
 
+    const visibleEvents = selectedEvents.slice(0, visibleCount);
+
+    useEffect(() => {
+        setVisibleCount(6);
+    }, [category]);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            entries => {
+                if (
+                    entries[0].isIntersecting && 
+                    visibleCount < selectedEvents.length
+                ) {
+                    setVisibleCount(prev => prev + 6);
+                }
+            },
+            {
+                threshold: 0.1,
+            }
+        );
+
+        const currentRef = loadMoreRef.current;
+
+        if (currentRef) {
+            observer.observe(currentRef);
+        }
+
+        return () => {
+            if (currentRef) {
+                observer.unobserve(currentRef);
+            }
+        };
+    }, [visibleCount, selectedEvents.length]);
+
     return (
         <div>
             <Title>Eventra</Title>
-
             <CategorySelect
                 category={category}
                 setCategory={setCategory}
             />
             <Cards>
-                {selectedEvents.map(event => (
+                {visibleEvents.map(event => (
                     <EventCard
                         key={event._id}
                         event={event}
                     />
                 ))}
             </Cards>
+            <div ref ={loadMoreRef}></div>
         </div>
     );
 }
